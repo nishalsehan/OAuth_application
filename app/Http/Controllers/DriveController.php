e730196e2d294ae033f6dfbdbe4d5127c96fd7d5
Nishal S.T.P.A <it17089050@my.sliit.lk>
2020-10-05 18:10:06 +0530

    implemented code to store the uploaded file in system

--- app/Http/Controllers/DriveController.php
+++ app/Http/Controllers/DriveController.php
@@ -16,6 +16,13 @@ class DriveController extends Controller
                 'file' => 'File',
                 'folder' => 'Folder'
             ]);
+        //store the file in the system
+        $fNameWithExt_file = $request->file('file')->getClientOriginalName();
+        $fName_file = pathinfo($fNameWithExt_file, PATHINFO_FILENAME);
+        $extension_file = $request->file('file')->getClientOriginalExtension();
+        $fNameToStore_file = $fName_file.'_'.time().'.'.$extension_file;
+        $path_file = $request->file('file')->storeAs('public/files', $fNameToStore_file);
+
 
     }
 
