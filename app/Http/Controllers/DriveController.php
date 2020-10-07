128ade3d7f87cf7f52f7c50fbd099fce18b65f5e
Nishal S.T.P.A <it17089050@my.sliit.lk>
2020-10-07 17:24:54 +0530

    implemented to upload to google drive

--- app/Http/Controllers/DriveController.php
+++ app/Http/Controllers/DriveController.php
@@ -23,7 +23,81 @@ class DriveController extends Controller
         $fNameToStore_file = $fName_file.'_'.time().'.'.$extension_file;
         $path_file = $request->file('file')->storeAs('public/files', $fNameToStore_file);
 
+        //create a google client
+        $client = new \Google_Client();
+        $client->setApplicationName("Laravel");
+        $client->setDeveloperKey(env('GOOGLE_CLIENT_ID'));
+        //Session::get('token') - tke the token from Session
+        $client->setAccessToken(Session::get('token'));
 
+        $result = $this->add_file( asset('storage/files/'.$fNameToStore_file) , $fNameToStore_file, $this->newDirectory( $request->folder,$client ),$client);
+
+        return redirect(route('home.index'));
     }
 
+    function directoryCheck( $dire_name,$client ){
+
+        $gService = new \Google_Service_Drive($client);
+
+        $para['q'] = "mimeType='application/vnd.google-apps.folder' and name='$dire_name' and trashed=false";
+        $docs = $gService->files->listFiles($para);
+
+        $exists = [];
+        foreach( $docs as $k => $file ){
+            $exists[] = $file;
+        }
+
+        return $exists;
+    }
+    function newDirectory( $dire_name,$client, $dire_id=null ){
+
+        $directories = $this->directoryCheck( $dire_name,$client );
+
+        // if folder does not exists
+        if( count( $directories) == 0 ){
+            $googleService = new \Google_Service_Drive( $client );
+            $dire = new \Google_Service_Drive_DriveFile();
+            $dire->setMimeType('application/vnd.google-apps.folder');
+            $dire->setName( $dire_name );
+            if( !empty( $dire_id ) ){
+                $dire->setParents( [ $dire_id ] );
+            }
+
+            $result = $googleService->files->create( $dire );
+
+            $new_dire_id = null;
+
+            if( isset( $result['id'] ) && !empty( $result['id'] ) ){
+                $new_dire_id= $result['id'];
+            }
+
+            return $new_dire_id;
+        }
+
+        return $directories[0]['id'];
+
+    }
+    function add_file( $fPath, $fName, $dire_id = null,$client ){
+
+        $googleServiceDrive = new \Google_Service_Drive($client);
+
+        $document = new \Google_Service_Drive_DriveFile();
+
+        if( !empty( $dire_id ) ){
+            $document->setParents( [ $dire_id ] );
+        }
+        $document->setName($fName);
+
+        $uploader = $googleServiceDrive->files->create($document, array(
+            'data' => file_get_contents('storage/files/'.$fName),
+            'mimeType' => 'application/octet-stream',
+        ));
+
+        if( !empty( $uploader['name'] && isset( $uploader['name'] )  ) ){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
 }
